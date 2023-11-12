dc2383ffe4919574065003a0b9553240e1d8aa17
Felix Cena Jr. <felixcena15@gmail.com>
2023-11-13 07:15:58 +0800

    done

---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'my_button.dart';
+
+class DialogBox extends StatelessWidget {
+  final controller;
+  VoidCallback onSave;
+  VoidCallback onCancel;
+
+  DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow[300],
+      content: SizedBox(
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Add a new task',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                MyButton(text: "Save", onPressed: onSave),
+                const SizedBox(width: 8),
+                MyButton(text: "Cancel", onPressed: onCancel),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
